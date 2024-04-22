45cf0a794ec475a9d4fe1ba5f04f2948bf794a19
GiorgioBelardelli <asterapocalypse@gmail.com>
2024-04-22 18:00:10 +0200

    Creazione libri e dvd

--- Program.cs
+++ Program.cs
@@ -1,10 +1,43 @@
-﻿namespace csharp_biblioteca
+﻿using static csharp_biblioteca.Prova;
+
+namespace csharp_biblioteca
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Biblioteca biblioteca = new Biblioteca();
+
+
+            /// UTENTI REGISTRATI
+            Utente utente1 = new Utente("Rossi", "Mario", "mario.rossi@example.com", "password123", "123456789");
+            biblioteca.AggiungiUtente(utente1);
+            Utente utente2 = new Utente("Verdi", "Anna", "anna.verdi@example.com", "password456", "987654321");
+            biblioteca.AggiungiUtente(utente2);
+            Utente utente3 = new Utente("Bianchi", "Luca", "luca.bianchi@example.com", "password789", "456789123");
+            biblioteca.AggiungiUtente(utente3);
+
+
+            ///CREAZIONE LIBRI E DVD
+
+            //libri
+            Libro libro1 = new Libro("L001", "Il Signore degli Anelli", 1954, "Fantasy", "A1", "J.R.R.Tolkien", 1000);
+            biblioteca.AggiungiDocumento(libro1);
+            Libro libro2 = new Libro("L002", "Cronache del ghiaccio e del fuoco", 1996, "Fantasy", "A2", "George R.R.Martin", 800);
+            biblioteca.AggiungiDocumento(libro2);
+            Libro libro3 = new Libro("L003", "1984", 1949, "Distopia", "B1", "George Orwell", 300);
+            biblioteca.AggiungiDocumento(libro3);
+
+            //dvd
+            DVD dvd1 = new DVD("D001", "Inception", 2010, "Thriller", "C1", "Christopher Nolan", 148);
+            biblioteca.AggiungiDocumento(dvd1);
+            DVD dvd2 = new DVD("D002", "Interstellar", 2014, "Fantascienza", "C2", "Christopher Nolan", 169);
+            biblioteca.AggiungiDocumento(dvd2);
+            DVD dvd3 = new DVD("D003", "La vita è bella", 1997, "Drammatico", "D1", "Roberto Benigni", 116);
+            biblioteca.AggiungiDocumento(dvd3);
+
         }
     }
+
+
 }
---
+++ Prova.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static csharp_biblioteca.Prova;
+
+namespace csharp_biblioteca
+{
+    internal class Prova
+    {
+        public class Biblioteca
+        {
+            private List<Documento> documenti;
+            private List<Utente> utenti;
+            private List<Prestito> prestiti;
+
+            public Biblioteca()
+            {
+                documenti = new List<Documento>();
+                utenti = new List<Utente>();
+                prestiti = new List<Prestito>();
+            }
+
+            public void AggiungiDocumento(Documento documento)
+            {
+                documenti.Add(documento);
+            }
+
+            public void AggiungiUtente(Utente utente)
+            {
+                utenti.Add(utente);
+            }
+
+            public void AggiungiPrestito(Utente utente, Documento documento, int DataInizio, int DataFine)
+            {
+                Prestito prestito = new Prestito(utente, documento, DataInizio, DataFine);
+                prestiti.Add(prestito);
+            }
+        }
+
+        public class Documento
+        {
+            private string _codice;
+            private string _titolo;
+            private int _anno;
+            private string _settore;
+            private string _scaffale;
+            private string _nomeautore;
+
+            public string Codice
+            {
+                get { return _codice; }
+                set { _codice = value; }
+            }
+            public string Titolo
+            {
+                get { return _titolo; }
+                set { _titolo = value; }
+            }
+            public int Anno
+            {
+                get { return _anno; }
+                set { _anno = value; }
+            }
+            public string Settore
+            {
+                get { return _settore; }
+                set { _settore = value; }
+            }
+            public string Scaffale
+            {
+                get { return _scaffale; }
+                set { _scaffale = value; }
+            }
+            public string NomeAutore
+            {
+                get { return _nomeautore; }
+                set { _nomeautore = value; }
+            }
+
+            public Documento(string Codice, string Titolo, int Anno, string Settore, string Scaffale, string NomeAutore)
+            {
+                _codice = Codice;
+                _titolo = Titolo;
+                _anno = Anno;
+                _settore = Settore;
+                _scaffale = Scaffale;
+                _nomeautore = NomeAutore;
+            }
+        }
+
+        public class Libro : Documento
+        {
+            public int _numeropagine;
+
+            public int NumeroPagine
+            {
+                get { return _numeropagine; }
+                set { _numeropagine = value; }
+            }
+
+            public Libro(string Codice, string Titolo, int Anno, string Settore, string Scaffale, string NomeAutore, int NumeroPagine)
+                : base(Codice, Titolo, Anno, Settore, Scaffale, NomeAutore)
+            {
+                NumeroPagine = _numeropagine;
+            }
+        }
+
+        public class DVD : Documento
+        {
+            public int _durata;
+
+            public int Durata
+            {
+                get { return _durata; }
+                set { _durata = value; }
+            }
+
+            public DVD(string Codice, string Titolo, int Anno, string Settore, string Scaffale, string NomeAutore, int Durata)
+                : base(Codice, Titolo, Anno, Settore, Scaffale, NomeAutore)
+            {
+                _durata = Durata;
+            }
+        }
+
+        public class Utente
+        {
+            private string _nome;
+            private string _cognome;
+            private string _email;
+            private string _password;
+            private string _telefono;
+
+            public string Nome
+            {
+                get { return _nome; }
+                set { _nome = value; }
+            }
+
+            public string Cognome
+            {
+                get { return _cognome; }
+                set { _cognome = value; }
+            }
+            public string Email
+            {
+                get { return _email; }
+                set { _email = value; }
+            }
+            public string Password
+            {
+                get { return _password; }
+                set { _password = value; }
+            }
+            public string Telefono
+            {
+                get { return _telefono; }
+                set { _telefono = value; }
+            }
+            public Utente(string Nome, string Cognome, string Email, string Password, string Telefono)
+            {
+                _nome = Nome;
+                _cognome = Cognome;
+                _email = Email;
+                _password = Password;
+                _telefono = Telefono;
+            }
+        }
+
+        public class Prestito
+        {
+            private Utente _utente;
+            private Documento _documento;
+            private int _datainizio;
+            private int _datafine;
+
+            public Utente Utente
+            {
+                get { return _utente; }
+            }
+            public Documento Documento
+            {
+                get { return _documento; }
+            }
+            public int DataInizio
+            {
+                get { return _datainizio; }
+                set { _datainizio = value; }
+            }
+            public int DataFine
+            {
+                get { return _datafine; }
+                set { _datafine = value; }
+            }
+
+            public Prestito(Utente Utente, Documento Documento, int DataInizio, int DataFine)
+            {
+                _utente = Utente;
+                _documento = Documento;
+                _datainizio = DataInizio;
+                _datafine = DataFine;
+            }
+        }
+    }
+}
